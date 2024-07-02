7ed3fa58898d79f0222cfea9060fc84560688c5b
Tshindimpho <mudzu13@gmail.com>
2024-07-02 16:55:50 +0200

    Add Application DB Context

---
+++ BookNest/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookNest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookNest.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+       
+    }
+}
